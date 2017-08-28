6eefddb4c888a95af5078ea381aa836fa479c629
Angelos Ikonomakis <angelos.ikonomakis@gmail.com>
2017-08-28 22:50:22 +0200

    calories and water consumption d3 visualization ready

--- d3_visualizations/calories/calories.js
+++ d3_visualizations/calories/calories.js
@@ -1,6 +1,6 @@
 function calories_graph(data) {
     // Define the margins of the svg
-    var margin = {top: 20, right: 20, bottom: 30, left: 50},
+    var margin = {top: 20, right: 30, bottom: 30, left: 50},
         width = 960 - margin.left - margin.right,
         height = 500 - margin.top - margin.bottom;
 
@@ -15,30 +15,45 @@ function calories_graph(data) {
           d.consumed = +d.consumed;
           d.burnt = +d.burnt;
           d.need = +d.need;
+          d.burnt_moves = +d.ttl_cal_burnt;
     }); 
     
     // Define the color scale
     var color = ["rgb(145,207,96)","rgb(252,141,89)"]
 
-    // Define the x-scale
+    // Define the x-scales
     var x = d3.scaleLinear()
               .range([0, width]);
 
-    // Define the y-scale
+    var xBar = d3.scaleBand()
+                .rangeRound([0, width])
+                .padding(0.1);
+
+    // Define the y-scales
     var y = d3.scaleLinear()
               .range([height, 0]);
+
+    var yBar = d3.scaleLinear()
+                .range([height, 0]);
 
     // Define the axis
     var xAxis = d3.axisBottom(x)
                   .tickValues(d3.range(1, 29, 1));
 
-    var yAxis = d3.axisLeft(y);
+    var yAxis = d3.axisLeft(y)
+                  .tickValues(d3.range(2000,5000,500));
+
+    var yAxisRight = d3.axisRight(yBar)
+                    .tickValues(d3.range(0.5,5.5,0.5)) // Define the limit of the water axis
+                    .tickFormat(d3.format(".1f"))
+                    .tickSize(-width+margin.right/2)
+                    .ticks(10);
 
     // Define the line of burnt (if the burnt is greater then consumed calories, we are possitive so we lose weight)
     var burntline = d3.area()
                     .curve(d3.curveBasis)
                     .x(function(d) { return x(d.day_no); })
-                    .y(function(d) { return y(d["burnt"]); });
+                    .y(function(d) { return y(d["burnt_moves"]); });
 
     // Define the line of consumed calories
     var consumedline = d3.area()
@@ -50,7 +65,7 @@ function calories_graph(data) {
     var burntarea = d3.area()
         .curve(d3.curveBasis)
         .x(function(d) { return x(d.day_no); })
-        .y1(function(d) { return y(d["burnt"]); });
+        .y1(function(d) { return y(d["burnt_moves"]); });
 
     // Initiate the svg and append it to div
     var svg = d3.select("#calories").append("svg")
@@ -60,11 +75,16 @@ function calories_graph(data) {
                 .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
     // Define the domains
-    x.domain(d3.extent(data, function(d) { return d.day_no; }));
-    y.domain([
-    d3.min(data, function(d) { return Math.min(d["consumed"], d["burnt"]); }),
-    d3.max(data, function(d) { return Math.max(d["consumed"], d["burnt"]); })
+    x.domain([
+        d3.min(data, function(d) { return d.day_no; })-1, //move the yaxis from the areagrahs
+        d3.max(data, function(d) { return d.day_no; })+1,
     ]);
+    y.domain([0,
+        //d3.min(data, function(d) { return Math.min(d["consumed"], d["burnt_moves"]); }),
+        d3.max(data, function(d) { return Math.max(d["consumed"], d["burnt_moves"]); })
+    ]);
+    xBar.domain(data.map(function(d,i) { return i-1; }));
+    yBar.domain([0, d3.max(data, function(d) { return d["water"]; }) + 7]); // take down the limits of the water y axis
 
     // Add the data to the whole svg
     svg.datum(data);
@@ -100,6 +120,16 @@ function calories_graph(data) {
         .attr("class", "line")
         .attr("d", consumedline)
         .style("stroke", "rgb(252,141,89)");
+
+    // The water bars
+    svg.selectAll(".bar")
+        .data(data)
+        .enter().append("rect")
+        .attr("class", "bar")
+        .attr("x", function(d, ix) { return x(d.day_no-0.45); }) // Centralized bars on the x-axis
+        .attr("width", xBar.bandwidth())
+        .attr("y", function(d) { return yBar(d["water"]); })
+        .attr("height", function(d) { return height - yBar(d["water"]); });
 
     // Build the legend
     var legend = svg.selectAll('g')
@@ -140,14 +170,29 @@ function calories_graph(data) {
         .call(xAxis);
 
     svg.append("g")
-        .attr("class", "y axis")
+        .attr("class", "y axis left")
         .call(yAxis)
         .append("text")
+        .attr("fill", "#000")
         .attr("transform", "rotate(-90)")
         .attr("y", 6)
         .attr("dy", ".71em")
+        .attr("dx", "-5em")
         .style("text-anchor", "end")
         .text("Calories (Kcal)");
 
-    
+    // Build y-axis right
+    svg.append("g")             
+        .attr("class", "y axis right")    
+        .attr("transform", "translate(" + width + " ,0)")    
+        .call(yAxisRight)
+        .append("text")
+        .attr("class", "right")
+        .attr("fill", "#000")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 1)
+        .attr("dy", "-.65em")
+        .attr("dx", "-30em")
+        .style("text-anchor", "end")
+        .text("Water Consumption(L)");
 }
